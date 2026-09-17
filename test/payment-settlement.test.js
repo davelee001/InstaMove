a4dba1ad154e56637652f2089489c055798c0a53
davelee001 <david.leekaleer@student.utamu.ac.ug>
2026-09-17 16:29:48 +0200

    Test rejection of oversized preimage in payment confirmation

--- test/payment-settlement.test.js
+++ test/payment-settlement.test.js
@@ -146,7 +146,8 @@ const invalidResponses = [
   ["conflicting code", { ...proof, code: 13 }],
   ["invalid base64", { ...proof, payment_preimage: "!".repeat(44) }],
   ["base64 with whitespace", { ...proof, payment_preimage: proof.payment_preimage + "\n" }],
-  ["truncated preimage", { ...proof, payment_preimage: Buffer.alloc(31, 7).toString("base64") }]
+  ["truncated preimage", { ...proof, payment_preimage: Buffer.alloc(31, 7).toString("base64") }],
+  ["oversized preimage", { ...proof, payment_preimage: Buffer.alloc(33, 7).toString("base64") }]
 ];
 
 for (const [name, response] of invalidResponses) {
