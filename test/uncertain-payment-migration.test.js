f90cb52930b54121e82dcfd75ffdcfd5f4667455
davelee001 <david.leekaleer@student.utamu.ac.ug>
2026-09-18 13:22:21 +0200

    Set up migration tests: const path = require("node:path");

--- test/uncertain-payment-migration.test.js
+++ test/uncertain-payment-migration.test.js
@@ -2,6 +2,7 @@ const assert = require("node:assert/strict");
 const crypto = require("node:crypto");
 const fs = require("node:fs");
 const os = require("node:os");
+const path = require("node:path");
 
 
 
